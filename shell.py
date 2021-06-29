bc504b9c55da3698541aac2de9dceb83ada57437
BizzyPythonBear <bizzeebear@hotmail.com>
2021-06-28 21:52:34 -0700

    Updated Shell.py

--- shell.py
+++ shell.py
@@ -2,7 +2,12 @@ import bearLang as bear
 
 while True:
     text = input('Bear Lang > ')
+    if text.strip() == "": continue
     result, error = bear.run('<stdin>', text)
-
-    if error: print(error.as_string())
-    else: print(result)
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
